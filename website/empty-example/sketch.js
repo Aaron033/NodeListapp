8737faca4d44ace521d5f74a980cfae9e3907f79
Aaron <aaron.botello.h@gmail.com>
2019-02-23 16:39:26 -0600

    code deleted 1.4

--- website/empty-example/sketch.js
+++ website/empty-example/sketch.js
@@ -1,11 +1,12 @@
 function setup() {
  createCanvas(400, 400);
     background(51);
-
+    loadJSON('/all', gotData); 
+    console.log('Running');
 }
 
-function setup() {
- createCanvas(400, 400);
-    background(51);
-
+function gotData(data){
+    console.log(data); 
 }
+
+
